43d1a69537d91965b1edc6d5e05c65fe646a3076
Kyuubi2709 <cryptokyuubi@protonmail.com>
2023-11-13 23:28:21 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,23 +2,28 @@
 
 set -e
 
-if [ "$1" = 'celestia' ]; then
-    echo "Initializing Celestia Node with command:"
-
+if [[ ! -f ${NODE_STORE}/init ||  ! -f /home/celestia/init ]]; then
     if [[ -n "$NODE_STORE" ]]; then
-        echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
-        celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
+        chmod 700 "${NODE_STORE}"
+        chown -R celestia:celestia "${NODE_STORE}"
+        if [[ ! -f ${NODE_STORE}/init ]]; then
+          echo "Initializing Celestia Node with command:"
+          echo "init" > ${NODE_STORE}/init
+          echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
+          celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
+        fi
     else
-        echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
-        celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
+       chmod 700 /home/celestia
+       chown -R celestia:celestia /home/celestia
+       if [[ ! -f /home/celestia/init ]]; then
+          echo "Initializing Celestia Node with command:"  
+          echo "init" > /home/celestia/init
+          echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
+          celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
+        fi
     fi
-
-    echo ""
     echo ""
 fi
 
-echo "Starting Celestia Node with command:"
-echo "$@"
-echo ""
-
-exec "$@"
+echo "Starting Celestia Node...."
+celestia $NODE_TYPE start
