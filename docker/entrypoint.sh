ac61f339b52aa3bed32fbbb577095ed629154231
Kyuubi2709 <cryptokyuubi@protonmail.com>
2023-11-13 23:39:22 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,28 +2,27 @@
 
 set -e
 
-if [[ ! -f ${NODE_STORE}/init ||  ! -f /home/celestia/init ]]; then
-    if [[ -n "$NODE_STORE" ]]; then
-        chmod 700 "${NODE_STORE}"
-        chown -R celestia:celestia "${NODE_STORE}"
-        if [[ ! -f ${NODE_STORE}/init ]]; then
-          echo "Initializing Celestia Node with command:"
-          echo "init" > ${NODE_STORE}/init
-          echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
-          celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
-        fi
-    else
-       chmod 700 /home/celestia
-       chown -R celestia:celestia /home/celestia
-       if [[ ! -f /home/celestia/init ]]; then
-          echo "Initializing Celestia Node with command:"  
-          echo "init" > /home/celestia/init
-          echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
-          celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
-        fi
+if [[ -n "$NODE_STORE" ]]; then
+    chmod 700 "${NODE_STORE}"
+    chown -R celestia:celestia "${NODE_STORE}"
+    if [[ ! -f ${NODE_STORE}/init ]]; then
+      echo "Initializing Celestia Node with command:"
+      echo "init" > ${NODE_STORE}/init
+      echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}""
+      celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}" --node.store "${NODE_STORE}"
     fi
-    echo ""
+else
+   chmod 700 /home/celestia
+   chown -R celestia:celestia /home/celestia
+   if [[ ! -f /home/celestia/init ]]; then
+      echo "Initializing Celestia Node with command:"  
+      echo "init" > /home/celestia/init
+      echo "celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}""
+      celestia "${NODE_TYPE}" init --p2p.network "${P2P_NETWORK}"
+    fi
 fi
 
+
+echo ""
 echo "Starting Celestia Node...."
 celestia $NODE_TYPE start
